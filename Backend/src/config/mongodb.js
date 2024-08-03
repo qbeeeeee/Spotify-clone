001834e9ad8329a62471429bdfe27923ab9e71fd
qbeeeeee <foxfireninetails9@gmail.com>
2024-08-03 17:43:10 +0300

    BackEnd-AddSong-ListSong

---
+++ Backend/src/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected',()=>{
+        console.log("Connection established");
+        
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
+}
+
+export default connectDB;
